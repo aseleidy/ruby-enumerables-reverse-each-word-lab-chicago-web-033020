6a29a1ad55c522f3b849396bf448cea526e0a3d9
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-26 19:36:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(string).each 
-  array = string.split(" ").map{|element| element.reverse}.join(" ")
+  string.split(" ").map{|element| element.reverse}.join(" ")
 
 end 
 
